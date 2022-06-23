e3ecf1fb9418c533fb3ba0dbfc7d986446963dcb
fractasy <fractasy@gmail.com>
2022-06-23 10:26:59 +0000

    Update zk-prover.proto, after removing execute and synchronize batch proposal methods

--- src/grpc/gen/zk-prover.grpc.pb.cc
+++ src/grpc/gen/zk-prover.grpc.pb.cc
@@ -27,8 +27,6 @@ static const char* ZKProverService_method_names[] = {
   "/zkprover.v1.ZKProverService/GenProof",
   "/zkprover.v1.ZKProverService/Cancel",
   "/zkprover.v1.ZKProverService/GetProof",
-  "/zkprover.v1.ZKProverService/Execute",
-  "/zkprover.v1.ZKProverService/SynchronizeBatchProposal",
 };
 
 std::unique_ptr< ZKProverService::Stub> ZKProverService::NewStub(const std::shared_ptr< ::grpc::ChannelInterface>& channel, const ::grpc::StubOptions& options) {
@@ -42,8 +40,6 @@ ZKProverService::Stub::Stub(const std::shared_ptr< ::grpc::ChannelInterface>& ch
   , rpcmethod_GenProof_(ZKProverService_method_names[1], ::grpc::internal::RpcMethod::NORMAL_RPC, channel)
   , rpcmethod_Cancel_(ZKProverService_method_names[2], ::grpc::internal::RpcMethod::NORMAL_RPC, channel)
   , rpcmethod_GetProof_(ZKProverService_method_names[3], ::grpc::internal::RpcMethod::BIDI_STREAMING, channel)
-  , rpcmethod_Execute_(ZKProverService_method_names[4], ::grpc::internal::RpcMethod::BIDI_STREAMING, channel)
-  , rpcmethod_SynchronizeBatchProposal_(ZKProverService_method_names[5], ::grpc::internal::RpcMethod::NORMAL_RPC, channel)
   {}
 
 ::grpc::Status ZKProverService::Stub::GetStatus(::grpc::ClientContext* context, const ::zkprover::v1::GetStatusRequest& request, ::zkprover::v1::GetStatusResponse* response) {
@@ -146,50 +142,6 @@ void ZKProverService::Stub::experimental_async::GetProof(::grpc::ClientContext* 
   return ::grpc_impl::internal::ClientAsyncReaderWriterFactory< ::zkprover::v1::GetProofRequest, ::zkprover::v1::GetProofResponse>::Create(channel_.get(), cq, rpcmethod_GetProof_, context, false, nullptr);
 }
 
-::grpc::ClientReaderWriter< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>* ZKProverService::Stub::ExecuteRaw(::grpc::ClientContext* context) {
-  return ::grpc_impl::internal::ClientReaderWriterFactory< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>::Create(channel_.get(), rpcmethod_Execute_, context);
-}
-
-void ZKProverService::Stub::experimental_async::Execute(::grpc::ClientContext* context, ::grpc::experimental::ClientBidiReactor< ::zkprover::v1::ExecuteRequest,::zkprover::v1::ExecuteResponse>* reactor) {
-  ::grpc_impl::internal::ClientCallbackReaderWriterFactory< ::zkprover::v1::ExecuteRequest,::zkprover::v1::ExecuteResponse>::Create(stub_->channel_.get(), stub_->rpcmethod_Execute_, context, reactor);
-}
-
-::grpc::ClientAsyncReaderWriter< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>* ZKProverService::Stub::AsyncExecuteRaw(::grpc::ClientContext* context, ::grpc::CompletionQueue* cq, void* tag) {
-  return ::grpc_impl::internal::ClientAsyncReaderWriterFactory< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>::Create(channel_.get(), cq, rpcmethod_Execute_, context, true, tag);
-}
-
-::grpc::ClientAsyncReaderWriter< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>* ZKProverService::Stub::PrepareAsyncExecuteRaw(::grpc::ClientContext* context, ::grpc::CompletionQueue* cq) {
-  return ::grpc_impl::internal::ClientAsyncReaderWriterFactory< ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>::Create(channel_.get(), cq, rpcmethod_Execute_, context, false, nullptr);
-}
-
-::grpc::Status ZKProverService::Stub::SynchronizeBatchProposal(::grpc::ClientContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest& request, ::zkprover::v1::SynchronizeBatchProposalResponse* response) {
-  return ::grpc::internal::BlockingUnaryCall(channel_.get(), rpcmethod_SynchronizeBatchProposal_, context, request, response);
-}
-
-void ZKProverService::Stub::experimental_async::SynchronizeBatchProposal(::grpc::ClientContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest* request, ::zkprover::v1::SynchronizeBatchProposalResponse* response, std::function<void(::grpc::Status)> f) {
-  ::grpc_impl::internal::CallbackUnaryCall(stub_->channel_.get(), stub_->rpcmethod_SynchronizeBatchProposal_, context, request, response, std::move(f));
-}
-
-void ZKProverService::Stub::experimental_async::SynchronizeBatchProposal(::grpc::ClientContext* context, const ::grpc::ByteBuffer* request, ::zkprover::v1::SynchronizeBatchProposalResponse* response, std::function<void(::grpc::Status)> f) {
-  ::grpc_impl::internal::CallbackUnaryCall(stub_->channel_.get(), stub_->rpcmethod_SynchronizeBatchProposal_, context, request, response, std::move(f));
-}
-
-void ZKProverService::Stub::experimental_async::SynchronizeBatchProposal(::grpc::ClientContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest* request, ::zkprover::v1::SynchronizeBatchProposalResponse* response, ::grpc::experimental::ClientUnaryReactor* reactor) {
-  ::grpc_impl::internal::ClientCallbackUnaryFactory::Create(stub_->channel_.get(), stub_->rpcmethod_SynchronizeBatchProposal_, context, request, response, reactor);
-}
-
-void ZKProverService::Stub::experimental_async::SynchronizeBatchProposal(::grpc::ClientContext* context, const ::grpc::ByteBuffer* request, ::zkprover::v1::SynchronizeBatchProposalResponse* response, ::grpc::experimental::ClientUnaryReactor* reactor) {
-  ::grpc_impl::internal::ClientCallbackUnaryFactory::Create(stub_->channel_.get(), stub_->rpcmethod_SynchronizeBatchProposal_, context, request, response, reactor);
-}
-
-::grpc::ClientAsyncResponseReader< ::zkprover::v1::SynchronizeBatchProposalResponse>* ZKProverService::Stub::AsyncSynchronizeBatchProposalRaw(::grpc::ClientContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest& request, ::grpc::CompletionQueue* cq) {
-  return ::grpc_impl::internal::ClientAsyncResponseReaderFactory< ::zkprover::v1::SynchronizeBatchProposalResponse>::Create(channel_.get(), cq, rpcmethod_SynchronizeBatchProposal_, context, request, true);
-}
-
-::grpc::ClientAsyncResponseReader< ::zkprover::v1::SynchronizeBatchProposalResponse>* ZKProverService::Stub::PrepareAsyncSynchronizeBatchProposalRaw(::grpc::ClientContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest& request, ::grpc::CompletionQueue* cq) {
-  return ::grpc_impl::internal::ClientAsyncResponseReaderFactory< ::zkprover::v1::SynchronizeBatchProposalResponse>::Create(channel_.get(), cq, rpcmethod_SynchronizeBatchProposal_, context, request, false);
-}
-
 ZKProverService::Service::Service() {
   AddMethod(new ::grpc::internal::RpcServiceMethod(
       ZKProverService_method_names[0],
@@ -231,26 +183,6 @@ ZKProverService::Service::Service() {
              ::zkprover::v1::GetProofRequest>* stream) {
                return service->GetProof(ctx, stream);
              }, this)));
-  AddMethod(new ::grpc::internal::RpcServiceMethod(
-      ZKProverService_method_names[4],
-      ::grpc::internal::RpcMethod::BIDI_STREAMING,
-      new ::grpc::internal::BidiStreamingHandler< ZKProverService::Service, ::zkprover::v1::ExecuteRequest, ::zkprover::v1::ExecuteResponse>(
-          [](ZKProverService::Service* service,
-             ::grpc_impl::ServerContext* ctx,
-             ::grpc_impl::ServerReaderWriter<::zkprover::v1::ExecuteResponse,
-             ::zkprover::v1::ExecuteRequest>* stream) {
-               return service->Execute(ctx, stream);
-             }, this)));
-  AddMethod(new ::grpc::internal::RpcServiceMethod(
-      ZKProverService_method_names[5],
-      ::grpc::internal::RpcMethod::NORMAL_RPC,
-      new ::grpc::internal::RpcMethodHandler< ZKProverService::Service, ::zkprover::v1::SynchronizeBatchProposalRequest, ::zkprover::v1::SynchronizeBatchProposalResponse>(
-          [](ZKProverService::Service* service,
-             ::grpc_impl::ServerContext* ctx,
-             const ::zkprover::v1::SynchronizeBatchProposalRequest* req,
-             ::zkprover::v1::SynchronizeBatchProposalResponse* resp) {
-               return service->SynchronizeBatchProposal(ctx, req, resp);
-             }, this)));
 }
 
 ZKProverService::Service::~Service() {
@@ -280,19 +212,6 @@ ZKProverService::Service::~Service() {
 ::grpc::Status ZKProverService::Service::GetProof(::grpc::ServerContext* context, ::grpc::ServerReaderWriter< ::zkprover::v1::GetProofResponse, ::zkprover::v1::GetProofRequest>* stream) {
   (void) context;
   (void) stream;
-  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, "");
-}
-
-::grpc::Status ZKProverService::Service::Execute(::grpc::ServerContext* context, ::grpc::ServerReaderWriter< ::zkprover::v1::ExecuteResponse, ::zkprover::v1::ExecuteRequest>* stream) {
-  (void) context;
-  (void) stream;
-  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, "");
-}
-
-::grpc::Status ZKProverService::Service::SynchronizeBatchProposal(::grpc::ServerContext* context, const ::zkprover::v1::SynchronizeBatchProposalRequest* request, ::zkprover::v1::SynchronizeBatchProposalResponse* response) {
-  (void) context;
-  (void) request;
-  (void) response;
   return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, "");
 }
 
