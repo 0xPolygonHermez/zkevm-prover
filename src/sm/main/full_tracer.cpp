abe40b81ca4944197b990cdc16bc993f56e6f31a
fractasy <fractasy@gmail.com>
2022-12-16 10:32:54 +0000

    Add new ROM error string to FullTracer

--- src/sm/main/full_tracer.cpp
+++ src/sm/main/full_tracer.cpp
@@ -42,6 +42,7 @@ set<string> responseErrors = {
     "intrinsic_invalid_chain_id",
     "intrinsic_invalid_nonce",
     "intrinsic_invalid_gas_limit",
+    "intrinsic_invalid_gas_overflow",
     "intrinsic_invalid_balance",
     "intrinsic_invalid_batch_gas_limit",
     "intrinsic_invalid_sender_code"};
