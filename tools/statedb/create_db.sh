6e7520470004cec2e5262a00a222365cd08b62d4
fractasy <fractasy@gmail.com>
2023-03-21 09:13:00 +0000

    Modify create_db.sh to accept a fourth argument to set the file.sql

--- tools/statedb/create_db.sh
+++ tools/statedb/create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ $# -ne 3 ]
+if [ $# -ne 3 ] && [ $# -ne 4 ]
     then
-        echo "usage: create_db <database> <user> <password>"
+        echo "usage: create_db <database> <user> <password> [<file.sql>]"
         exit
 fi
 echo "StateDB database creation"
@@ -16,6 +16,11 @@ echo "Creating table state.merkletree..."
 PGPASSWORD=$3 psql -U $2 -h 127.0.0.1 -p 5432 -d $1 -c 'create schema state;'
 PGPASSWORD=$3 psql -U $2 -h 127.0.0.1 -p 5432 -d $1 -c 'create table state.nodes (hash bytea primary key, data bytea not null);'
 PGPASSWORD=$3 psql -U $2 -h 127.0.0.1 -p 5432 -d $1 -c 'create table state.program (hash bytea primary key, data bytea not null);'
+if [ $# == 4 ]
+    then
+        PGPASSWORD=$3 psql -U $2 -h 127.0.0.1 -p 5432 -d $1 -f $4
+fi
+
 echo "Done."
 echo "Example of connection string to use in the config.json file:"
 echo "  \"databaseURL\": \"postgresql://$2:$3@127.0.0.1:5432/$1\""
