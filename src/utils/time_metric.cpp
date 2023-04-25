9fb2021397039e12bb7841d89a6cd1b05f4e184e
fractasy <fractasy@gmail.com>
2023-04-25 17:43:54 +0000

    Add TIME_METRIC_TABLE definition to control TimeMetric log format

--- src/utils/time_metric.cpp
+++ src/utils/time_metric.cpp
@@ -31,13 +31,19 @@ void TimeMetricStorage::print(const char * pTitle, uint64_t padding)
 
     uint64_t duration = TimeDiff(startTime);
 
+    string s;
+
     if (pTitle != NULL)
     {
-        zklog.info(string(pTitle) + ":");
+        s = string(pTitle) + ": ";
+
+#ifdef TIME_METRIC_TABLE
+        s += "\n";
+#endif
     }
     else
     {
-        zklog.info("TimeMetricStorage::print():");
+        s = "TimeMetricStorage::print(): ";
     }
     
     uint64_t totalTime = 0;
@@ -49,69 +55,100 @@ void TimeMetricStorage::print(const char * pTitle, uint64_t padding)
         totalTimes += it->second.times;
     }
 
-    zklog.info("Duration=" + to_string(duration) + " us time/duration=" + to_string(totalTime*1000/zkmax(duration, (uint64_t)1)) + "%");
+    s += "Duration=" + to_string(duration) + "us time/duration=" + to_string(totalTime*1000/zkmax(duration, (uint64_t)1)) + "% ";
+
+#ifdef TIME_METRIC_TABLE
+    s += "\n";
+#endif
 
     for (it = map.begin(); it != map.end(); it++)
     {
         string key = it->first;
+#ifdef TIME_METRIC_TABLE
         if (key.size() < padding)
         {
             key.insert(0, padding - key.size(), ' ');
         }
+#endif
         string time = to_string(it->second.time);
+#ifdef TIME_METRIC_TABLE
         if (time.size() < 10)
         {
             time.insert(0, 10 - time.size(), ' ');
         }
+#endif
         string percentage1 = to_string(it->second.time*1000/zkmax(totalTime,(uint64_t)1));
+#ifdef TIME_METRIC_TABLE
         if (percentage1.size() < 3)
         {
             percentage1.insert(0, 3 - percentage1.size(), ' ');
         }
+#endif
         string percentage2 = to_string(it->second.time*1000/zkmax(duration,(uint64_t)1));
+#ifdef TIME_METRIC_TABLE
         if (percentage2.size() < 3)
         {
             percentage2.insert(0, 3 - percentage2.size(), ' ');
         }
+#endif
         string times = to_string(it->second.times);
+#ifdef TIME_METRIC_TABLE
         if (times.size() < 8)
         {
             times.insert(0, 8 - times.size(), ' ');
         }
+#endif
         string nsPerTime = to_string(it->second.time*1000/zkmax(it->second.times,(uint64_t)1));
+#ifdef TIME_METRIC_TABLE
         if (nsPerTime.size() < 7)
         {
             nsPerTime.insert(0, 7 - nsPerTime.size(), ' ');
         }
-        zklog.info(key + " time: " + time + " us (" + percentage1 + "%)(" + percentage2 + "%), called " + times + " times, so " + nsPerTime + " ns/time");
+#endif
+        s += key + "=" + time + "us(" + percentage1 + "%)(" + percentage2 + "%)=" + times + "times=" + nsPerTime + "ns/time ";
+#ifdef TIME_METRIC_TABLE
+        s += "\n";
+#endif
     }
     string total = "TOTAL";
+#ifdef TIME_METRIC_TABLE
     if(total.size() < padding)
     {
         total.insert(0, padding - total.size(), ' ');
     }
+#endif
     string totalTimeString = to_string(totalTime);
+#ifdef TIME_METRIC_TABLE
     if (totalTimeString.size() < 10)
     {
         totalTimeString.insert(0, 10 - totalTimeString.size(), ' ');
     }
+#endif
     string percentage = to_string(totalTime*1000/zkmax(duration, (uint64_t)1));
+#ifdef TIME_METRIC_TABLE
     if (percentage.size() < 3)
     {
         percentage.insert(0, 3 - percentage.size(), ' ');
     }
+#endif
     string totalTimesString = to_string(totalTimes);
+#ifdef TIME_METRIC_TABLE
     if (totalTimesString.size() < 8)
     {
         totalTimesString.insert(0, 8 - totalTimesString.size(), ' ');
     }
+#endif
     string totalNsPerTime = to_string(totalTime*1000/zkmax(totalTimes,(uint64_t)1));
+#ifdef TIME_METRIC_TABLE
     if (totalNsPerTime.size() < 7)
     {
         totalNsPerTime.insert(0, 7 - totalNsPerTime.size(), ' ');
     }
+#endif
 
-    zklog.info(total + " time: " + totalTimeString + " us (1000)(" + percentage + "%), called " + totalTimesString + " times, so " + totalNsPerTime + " ns/time");
+    s += total + "=" + totalTimeString + "us(1000)(" + percentage + "%)=" + totalTimesString + "times=" + totalNsPerTime + "ns/time";
+
+    zklog.info(s);
 
     unlock();
 }
