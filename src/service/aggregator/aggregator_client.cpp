8fd382139a4d48f6fa81f912b266646cd6c58297
Eduardo Antuña <eduadiez@gmail.com>
2023-06-07 17:22:36 +0200

    add exit 0 to Control the number of streams does not exceed the maximum

--- src/service/aggregator/aggregator_client.cpp
+++ src/service/aggregator/aggregator_client.cpp
@@ -557,7 +557,7 @@ void* aggregatorClientThread(void* arg)
             zklog.info("aggregatorClientThread() killing process since we reached the maximum number of streams=" + to_string(pAggregatorClient->config.aggregatorClientMaxStreams));
             zklog.info("Waiting 5 min before killing it...");
             sleep(300);
-            exitProcess();
+            exit(0);
         }
         numberOfStreams++;
 
