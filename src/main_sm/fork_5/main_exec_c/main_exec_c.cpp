6050cb1205cda1d1211df6014c2b7c3e0e8eff28
fractasy <fractasy@gmail.com>
2023-06-22 10:40:29 +0000

    Implement Account::GetBalance()

--- src/main_sm/fork_5/main_exec_c/main_exec_c.cpp
+++ src/main_sm/fork_5/main_exec_c/main_exec_c.cpp
@@ -2,6 +2,7 @@
 #include "main_sm/fork_5/main_exec_c/context_c.hpp"
 #include "main_sm/fork_5/main_exec_c/variables_c.hpp"
 #include "main_sm/fork_5/main_exec_c/batch_decode.hpp"
+#include "main_sm/fork_5/main_exec_c/account.hpp"
 #include "main_sm/fork_5/main/eval_command.hpp"
 #include "main_sm/fork_5/main/context.hpp"
 #include "scalar.hpp"
@@ -112,6 +113,8 @@ void MainExecutorC::execute (ProverRequest &proverRequest)
 
     // Set initial state root
     ctxc.regs.SR = proverRequest.input.publicInputsExtended.publicInputs.oldStateRoot;
+    Goldilocks::Element oldRoot[4];
+    scalar2fea(fr, proverRequest.input.publicInputsExtended.publicInputs.oldStateRoot, oldRoot); // TODO: Check range?
 
     // Set oldAccInputHash
     ctxc.globalVars.oldAccInputHash = proverRequest.input.publicInputsExtended.publicInputs.oldAccInputHash;
@@ -167,14 +170,93 @@ void MainExecutorC::execute (ProverRequest &proverRequest)
 
         // Verify signature and obtain public from key
         //ecRecover(r, s, v, hash) -> obtain public key
+        mpz_class fromPublicKey;  // TODO: Call ecrecover()
 
         // from.account.balance -= value + gas*gasPrice;
-        //SMT.get -> balance > x
-        //SMT.set -> balance = y = x - value - gas*gasPrice
+        Account fromAccount(fr, poseidon, *pHashDB);
+        result = fromAccount.Init(fromPublicKey);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling fromAccount.Init()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+        Account toAccount(fr, poseidon, *pHashDB);
+        result = toAccount.Init(batchData.tx[tx].to);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling toAccount.Init()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
 
-        // to.account.balance += value
-        //SMT.get -> balance = x
-        //SMT.set -> balance = y = x + value
+        // Get balance of from account
+        mpz_class fromBalance;
+        result = fromAccount.GetBalance(oldRoot, fromBalance);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling fromAccount.GetBalance()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+
+        // Get balance of to account
+        mpz_class toBalance;
+        result = toAccount.GetBalance(oldRoot, toBalance);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling toAccount.GetBalance()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+
+        // Calculate gas
+        mpz_class gas = 21000; // Transfer with no data, no CALLDATA cost, no deployment cost
+
+        // Check that gas is not higher than gas limit
+        if (gas > batchData.tx[tx].gasLimit)
+        {
+            zklog.error("main_exec_c() failed gas=" + gas.get_str(10) + " < gasLimit=" + to_string(batchData.tx[tx].gasLimit));
+            proverRequest.result = ZKR_UNSPECIFIED;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+
+        // Check that from account has enough balance to complete the transfer
+        mpz_class fromAmount = batchData.tx[tx].value + batchData.tx[tx].gasPrice*gas;
+        if (fromBalance < fromAmount)
+        {
+            zklog.error("main_exec_c() failed fromBalance=" + fromBalance.get_str(10) + " < fromAmount=" + fromAmount.get_str(10));
+            proverRequest.result = ZKR_UNSPECIFIED;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+
+        // Update from account balance = balance - value - gas*gasPrice
+        fromBalance -= fromAmount;
+        result = fromAccount.SetBalance(fromBalance);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling fromAccount.SetBalance()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
+
+        // Update to account balance = balance + value
+        toBalance += batchData.tx[tx].value;
+        result = toAccount.SetBalance(toBalance);
+        if (result != ZKR_SUCCESS)
+        {
+            zklog.error("main_exec_c() failed calling toAccount.SetBalance()");
+            proverRequest.result = result;
+            HashDBClientFactory::freeHashDBClient(pHashDB);
+            return;
+        }
     }
 
     /*result = ((fork_5::FullTracer *)ctx.proverRequest.pFullTracer)->onFinishBatch(ctx, cmd);
@@ -186,134 +268,9 @@ void MainExecutorC::execute (ProverRequest &proverRequest)
         return;
     }*/
 
+    HashDBClientFactory::freeHashDBClient(pHashDB);
     proverRequest.result = ZKR_SUCCESS;
     TimerStopAndLog(MAIN_EXEC_C);
-}
-
-zkresult MainExecutorC::SLOAD(Context &ctx, ContextC &ctxc, const mpz_class &a, const mpz_class &b, const mpz_class &c, mpz_class &value)
-{
-    Goldilocks::Element A[8];
-    scalar2fea(fr, a, A);
-    Goldilocks::Element B[8];
-    scalar2fea(fr, b, B);
-    Goldilocks::Element C[8];
-    scalar2fea(fr, c, C);
-
-    Goldilocks::Element Kin0[12];
-    Kin0[0] = C[0];
-    Kin0[1] = C[1];
-    Kin0[2] = C[2];
-    Kin0[3] = C[3];
-    Kin0[4] = C[4];
-    Kin0[5] = C[5];
-    Kin0[6] = C[6];
-    Kin0[7] = C[7];
-    Kin0[8] = fr.zero();
-    Kin0[9] = fr.zero();
-    Kin0[10] = fr.zero();
-    Kin0[11] = fr.zero();
-
-    Goldilocks::Element Kin1[12];
-    Kin1[0] = A[0];
-    Kin1[1] = A[1];
-    Kin1[2] = A[2];
-    Kin1[3] = A[3];
-    Kin1[4] = A[4];
-    Kin1[5] = A[5];
-    Kin1[6] = B[0];
-    Kin1[7] = B[1];
-
-    if  ( !fr.isZero(A[5]) || !fr.isZero(A[6]) || !fr.isZero(A[7]) || !fr.isZero(B[2]) || !fr.isZero(B[3]) || !fr.isZero(B[4]) || !fr.isZero(B[5])|| !fr.isZero(B[6])|| !fr.isZero(B[7]) )
-    {
-        zklog.error("MainExecutorC::SLOAD() found non-zero A-B storage registers");
-        return ZKR_SM_MAIN_STORAGE;
-    }
-
-    // Call poseidon and get the hash key
-    Goldilocks::Element Kin0Hash[4];
-    poseidon.hash(Kin0Hash, Kin0);
-
-    // Reinject the first resulting hash as the capacity for the next poseidon hash
-    Kin1[8] = Kin0Hash[0];
-    Kin1[9] = Kin0Hash[1];
-    Kin1[10] = Kin0Hash[2];
-    Kin1[11] = Kin0Hash[3];
-
-    // Call poseidon hash
-    Goldilocks::Element Kin1Hash[4];
-    poseidon.hash(Kin1Hash, Kin1);
-
-    Goldilocks::Element key[4];
-    key[0] = Kin1Hash[0];
-    key[1] = Kin1Hash[1];
-    key[2] = Kin1Hash[2];
-    key[3] = Kin1Hash[3];
-
-    Goldilocks::Element oldRoot[4];
-    scalar2fea(fr, ctxc.regs.SR, oldRoot);
-
-    //SmtGetResult smtGetResult;
-    //mpz_class value;
-    zkresult zkResult = ctx.pHashDB->get(oldRoot, key, value, /*&smtGetResult*/NULL, ctx.proverRequest.dbReadLog);
-    if (zkResult != ZKR_SUCCESS)
-    {
-        zklog.error("MainExecutorC::SLOAD() failed calling pHashDB->get() result=" + zkresult2string(zkResult));
-        return zkResult;
-    }
-    //incCounter = smtGetResult.proofHashCounter + 2;
-                    //cout << "smt.get() returns value=" << smtGetResult.value.get_str(16) << endl;
-
-    /*if (bProcessBatch)
-    {
-        zkResult = eval_addReadWriteAddress(ctx, smtGetResult.value);
-        if (zkResult != ZKR_SUCCESS)
-        {
-            proverRequest.result = zkResult;
-            logError(ctx, string("Failed calling eval_addReadWriteAddress() 1 result=") + zkresult2string(zkResult));
-            HashDBClientFactory::freeHashDBClient(pHashDB);
-            return;
-        }
-    }*/
-/*
-
-                    SmtGetResult smtGetResult;
-                    mpz_class value;
-                    zkresult zkResult = pHashDB->get(oldRoot, key, value, &smtGetResult, proverRequest.dbReadLog);
-                    if (zkResult != ZKR_SUCCESS)
-                    {
-                        proverRequest.result = zkResult;
-                        logError(ctx, string("Failed calling pHashDB->get() result=") + zkresult2string(zkResult));
-                        HashDBClientFactory::freeHashDBClient(pHashDB);
-                        return;
-                    }
-                    incCounter = smtGetResult.proofHashCounter + 2;
-                    //cout << "smt.get() returns value=" << smtGetResult.value.get_str(16) << endl;
-
-                    if (bProcessBatch)
-                    {
-                        zkResult = eval_addReadWriteAddress(ctx, smtGetResult.value);
-                        if (zkResult != ZKR_SUCCESS)
-                        {
-                            proverRequest.result = zkResult;
-                            logError(ctx, string("Failed calling eval_addReadWriteAddress() 1 result=") + zkresult2string(zkResult));
-                            HashDBClientFactory::freeHashDBClient(pHashDB);
-                            return;
-                        }
-                    }
-
-#ifdef LOG_TIME_STATISTICS_MAIN_EXECUTOR
-                    mainMetrics.add("SMT Get", TimeDiff(t));
-#endif
-
-                    scalar2fea(fr, smtGetResult.value, fi0, fi1, fi2, fi3, fi4, fi5, fi6, fi7);
-
-                    nHits++;
-#ifdef LOG_STORAGE
-                    zklog.info("Storage read sRD read from key: " + ctx.fr.toString(ctx.lastSWrite.key, 16) + " value:" + fr.toString(fi3, 16) + ":" + fr.toString(fi2, 16) + ":" + fr.toString(fi1, 16) + ":" + fr.toString(fi0, 16));
-#endif
-
-*/
-    return ZKR_SUCCESS;
 }
 
 }
