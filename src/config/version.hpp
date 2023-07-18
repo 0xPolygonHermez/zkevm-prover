7ce8acf451c923d36e5a55a8553f52bd2f728c30
fractasy <fractasy@gmail.com>
2023-07-18 11:49:32 +0000

    Change version to v2.0.0-RC5-verbose.2

--- src/config/version.hpp
+++ src/config/version.hpp
@@ -1,6 +1,6 @@
 #ifndef ZKEVM_PROVER_VERSION_HPP
 #define ZKEVM_PROVER_VERSION_HPP
 
-#define ZKEVM_PROVER_VERSION "v2.0.0-RC5-verbose"
+#define ZKEVM_PROVER_VERSION "v2.0.0-RC5-verbose.2"
 
 #endif
