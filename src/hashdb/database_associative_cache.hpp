dda223f661d1009cdb262d77830949c79290d7a1
rickb80 <rickborrell@gmail.com>
2023-09-30 10:53:42 +0200

    Update associative cache from develop

--- src/hashdb/database_associative_cache.hpp
+++ src/hashdb/database_associative_cache.hpp
@@ -8,48 +8,53 @@
 #include "zkmax.hpp"
 
 using namespace std;
-using json = nlohmann::json;
 class DatabaseMTAssociativeCache
 {
-    private:
-        recursive_mutex mlock;
+private:
+    recursive_mutex mlock;
 
-        int nKeyBits;
-        uint32_t indexesSize;
-        int log2CacheSize;
-        uint32_t cacheSize;
+    int log2IndexesSize;
+    uint32_t indexesSize;
+    int log2CacheSize;
+    uint32_t cacheSize;
 
-        uint32_t *indexes;
-        Goldilocks::Element *keys;
-        Goldilocks::Element *values;
-        bool *isLeaf;
-        uint32_t currentCacheIndex; 
+    uint32_t *indexes;
+    Goldilocks::Element *keys;
+    Goldilocks::Element *values;
+    uint32_t currentCacheIndex;
 
-        uint64_t attempts;
-        uint64_t hits;
-        string name;
+    uint64_t attempts;
+    uint64_t hits;
+    string name;
 
-        uint64_t indexesMask;
-        uint64_t cacheMask;
+    uint64_t indexesMask;
+    uint64_t cacheMask;
 
+public:
+    DatabaseMTAssociativeCache();
+    DatabaseMTAssociativeCache(int log2IndexesSize_, int log2CacheSize_, string name_);
+    ~DatabaseMTAssociativeCache();
 
-    public:
+    void postConstruct(int log2IndexesSize_, int log2CacheSize_, string name_);
+    void addKeyValue(Goldilocks::Element (&key)[4], const vector<Goldilocks::Element> &value, bool update);
+    bool findKey(const Goldilocks::Element (&key)[4], vector<Goldilocks::Element> &value);
+    inline bool enabled() const { return (log2IndexesSize > 0); };
+    inline uint32_t getCacheSize() const { return cacheSize; };
+    inline uint32_t getIndexesSize() const { return indexesSize; };
+    inline void clear()
+    {
+        if (enabled())
+        {
+            postConstruct(log2IndexesSize, log2CacheSize, name);
+        }
+    }
 
-        DatabaseMTAssociativeCache();
-        DatabaseMTAssociativeCache(int nKeyBits_, int log2CacheSize_, string name_);
-        ~DatabaseMTAssociativeCache();
-
-        void postConstruct(int nKeyBits_, int log2CacheSize_, string name_);
-        void addKeyValue(Goldilocks::Element (&key)[4], const vector<Goldilocks::Element> &value, bool update);
-        bool findKey(Goldilocks::Element (&key)[4], vector<Goldilocks::Element> &value);
-        inline bool enabled() const { return (nKeyBits > 0); };
-        inline uint32_t getCacheSize()  const { return cacheSize; };
-        inline uint32_t getIndexesSize() const { return indexesSize; };
-
-    private:
-        void forcedInsertion(uint32_t (&rawCacheIndexes)[10], int &iters);
+private:
+    inline bool emptyCacheSlot(uint32_t cacheIndexRaw) const
+    {
+        return (currentCacheIndex >= cacheIndexRaw && currentCacheIndex - cacheIndexRaw > cacheSize) ||
+               (currentCacheIndex < cacheIndexRaw && UINT32_MAX - cacheIndexRaw + currentCacheIndex > cacheSize);
+    };
+    void forcedInsertion(uint32_t (&usedRawCacheIndexes)[10], int &iters);
 };
 #endif
-
-// TODO:
-// 5. Put attempts and hits in a higher level
