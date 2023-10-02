545d85622b2d0adb68975aa5dfbd0874f1c8599e
Ricard Borrell <75077385+rickb80@users.noreply.github.com>
2023-10-02 12:20:40 +0000

    Recover proper page in the InitPageEmpty

--- src/hashdb64/page/header_page.cpp
+++ src/hashdb64/page/header_page.cpp
@@ -2,10 +2,11 @@
 #include "zklog.hpp"
 #include "exit_process.hpp"
 #include "zkassert.hpp"
+#include "page_manager.hpp"
 
 zkresult HeaderPage::InitEmptyPage (const uint64_t pageNumber)
 {
-    HeaderStruct * page = (HeaderStruct *)(pageNumber*4096);
+    HeaderStruct * page = (HeaderStruct *)(pageManager.getPage(pageNumber));
     page->rootVersionPage = 0;
     return ZKR_SUCCESS;
 }
