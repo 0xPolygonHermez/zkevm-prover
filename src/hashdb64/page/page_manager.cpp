5a555a27fd83f671118c3225a41e59b1b8983f1c
rickb80 <rickborrell@gmail.com>
2023-10-03 20:05:38 +0200

    Add editedPaged and flushPages into PageManager

--- src/hashdb64/page/page_manager.cpp
+++ src/hashdb64/page/page_manager.cpp
@@ -3,16 +3,13 @@
 #include "exit_process.hpp"
 #include "zkassert.hpp"
 #include <cstring>
-
 PageManager pageManager;
-
 PageManager::PageManager()
 {
     nPages = 0;
     pages = NULL;
-    AddPages(1024);
+    AddPages(10240);
 }
-
 PageManager::PageManager(const uint64_t nPages_)
 {
     nPages = 0;
@@ -27,14 +24,14 @@ PageManager::~PageManager(void)
 zkresult PageManager::AddPages(const uint64_t nPages_)
 {
     char *auxPages = NULL;
-    auxPages = (char *)realloc(pages, nPages + nPages_ * 4096);
+    auxPages = (char *)realloc(pages, (nPages + nPages_) * 4096);
     if (auxPages != NULL)
     {
         pages = auxPages;
         memset(pages + nPages * 4096, 0, nPages_ * 4096);
-        nPages += nPages_;
         for (uint64_t i = 0; i < nPages_; i++)
             freePages.push_back(nPages + i);
+        nPages += nPages_;
     }
     else
     {
@@ -43,16 +40,37 @@ zkresult PageManager::AddPages(const uint64_t nPages_)
     }
     return zkresult::ZKR_SUCCESS;
 }
-
 uint64_t PageManager::getFreePage(void)
 {
+    if(freePages.empty())
+    {
+        AddPages(1024);
+    }
     uint32_t pageNumber = freePages.front();
     freePages.pop_front();
     return pageNumber;
 }
-
 void PageManager::releasePage(const uint64_t pageNumber)
 {
-    memset(getPage(pageNumber), 0, 4096);
+    memset(getPageAddress(pageNumber), 0, 4096);
     freePages.push_back(pageNumber);
+}
+
+uint32_t PageManager::editPage(const uint32_t pageNumber)
+{
+    std::pair<std::unordered_set<uint32_t>::iterator,bool> res = editedPages.insert(pageNumber);
+    uint32_t pageNumber_ = pageNumber;
+    if(res.second == true)
+    {
+        pageNumber_ = getFreePage();
+        memcpy(getPageAddress(pageNumber_),getPageAddress(pageNumber) , 4096);
+    }
+    return pageNumber_;
+}
+
+void PageManager::flushPages(){
+    for(unordered_set<uint32_t>::const_iterator it = editedPages.begin(); it != editedPages.end(); it++){
+        releasePage(*it);
+    }
+    editedPages.clear();
 }
--- src/hashdb64/page/page_manager.hpp
+++ src/hashdb64/page/page_manager.hpp
@@ -6,6 +6,7 @@
 #include "exit_process.hpp"
 #include "zkresult.hpp"
 #include <list>
+#include <unordered_set>
 
 class PageManager
 {
@@ -13,19 +14,26 @@ public:
     PageManager();
     PageManager(const uint64_t nPages_);
     ~PageManager();
-    zkresult AddPages(const uint64_t nPages_);
-    inline char *getPage(const uint64_t pageNumber)
+    inline char *getPageAddress(const uint64_t pageNumber)
     {
         return pages + pageNumber * 4096;
     };
 
     uint64_t getFreePage();
     void releasePage(const uint64_t pageNumber);
+    uint32_t editPage(const uint32_t pageNumber);
+    void flushPages();
+
 
 private:
     uint32_t nPages;
     char *pages;
+    
     std::list<uint32_t> freePages;
+    std::unordered_set<uint32_t> editedPages;
+
+    zkresult AddPages(const uint64_t nPages_);
+
 };
 
 extern PageManager pageManager;
