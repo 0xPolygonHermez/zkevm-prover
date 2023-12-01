b31e14452e518a6c056813b55995b5cac198ae33
rickb80 <rickborrell@gmail.com>
2023-12-01 13:13:09 +0100

    Substitue Keccak funciton in PageContext

--- src/hashdb64/page/page_context.hpp
+++ src/hashdb64/page/page_context.hpp
@@ -4,7 +4,7 @@
 
 #include "page_manager.hpp"
 #include "config.hpp"
-#include "keccak.hpp"
+#include "Keccak-more-compact.hpp"
 
 class PageContext
 {
@@ -17,8 +17,7 @@ public:
     PageContext (PageManager &pageManager_, const Config &config_ ) :
         pageManager(pageManager_), config(config_) {
             string uuidString = "Polygon zkEVM HashDB64 v1.0.0";
-            Keccak((uint8_t *)uuidString.c_str(), uuidString.size(), uuid);       
+            FIPS202_SHA3_256((uint8_t *)uuidString.c_str(), uuidString.size(), uuid);
         }; 
-
 };
 #endif
