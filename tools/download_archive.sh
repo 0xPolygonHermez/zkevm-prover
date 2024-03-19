a50265387d798b7d7fa2766ad1ae2de378f82889
kylegranger <kyle.robert.granger@gmail.com>
2024-03-19 08:55:13 +0100

    shim + new download

--- tools/download_archive.sh
+++ tools/download_archive.sh
@@ -5,7 +5,7 @@ echo "Please ensure that you run this script from the root of the zkevm-prover r
 echo "Press Enter to continue..."
 read
 
-ARCHIVE_NAME="v3.0.0-RC3-fork.6"
+ARCHIVE_NAME="v4.0.0-rc.6-fork.7"
 ARCHIVE_EXTENSION=".tgz"
 ARCHIVE_URL="https://storage.googleapis.com/zkevm/zkproverc/${ARCHIVE_NAME}${ARCHIVE_EXTENSION}"
 
