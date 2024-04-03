a18d4092ff3de5973e5446a017092a543e3b0ae6
kylegranger <kyle.robert.granger@gmail.com>
2024-04-03 10:13:50 +0200

    build fix

--- src/main.cpp
+++ src/main.cpp
@@ -890,7 +890,7 @@ void* run_gevulot(const struct Task* task) {
 
 void show_dir_content(char * path)
 {
-  printf("content for %s\n", path);
+  printf("show_dir_content for %s\n", path);
 
     DIR * d = opendir(path); // open the path
   if(d==NULL) return; // if was not able, return
@@ -898,11 +898,11 @@ void show_dir_content(char * path)
   while ((dir = readdir(d)) != NULL) // if we were able to read somehting from the directory
     {
       if(dir-> d_type != DT_DIR) // if the type is not directory just print it with blue color
-        printf("  %s\n", dir->d_name);
+        printf("%s\n", dir->d_name);
       else
       if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) // if it is a directory
       {
-        printf("  %s\n", dir->d_name); // print its name in green
+        printf("%s\n", dir->d_name); // print its name in green
         char d_path[255]; // here I am using sprintf which is safer than strcat
         sprintf(d_path, "%s/%s", path, dir->d_name);
         show_dir_content(d_path); // recall with the new path
