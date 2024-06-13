4e243ba18422bd0e765fee414bb5edb4169b60f4
fractasy <fractasy@gmail.com>
2024-06-13 10:07:45 +0000

    Make ZKEVM_PROVER_VERSION fork-dependant

--- src/config/version.hpp
+++ src/config/version.hpp
@@ -1,6 +1,14 @@
 #ifndef ZKEVM_PROVER_VERSION_HPP
 #define ZKEVM_PROVER_VERSION_HPP
 
-#define ZKEVM_PROVER_VERSION "v7.0.3-RC0"
+#include "definitions.hpp"
+
+#if (PROVER_FORK_ID==10)
+#define ZKEVM_PROVER_VERSION "v7.0.0-RC0-fork.10"
+#elif (PROVER_FORK_ID==11)
+#define ZKEVM_PROVER_VERSION "v7.0.0-RC0-fork.11"
+#else
+#error "Invalid PROVER_FORK_ID"
+#endif
 
 #endif
