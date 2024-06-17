3c401a933c18812aa9abcabc6769db3f9b2cbc3d
fractasy <fractasy@gmail.com>
2024-06-17 20:12:58 +0000

    Change version to v7.0.0-RC1

--- src/config/version.hpp
+++ src/config/version.hpp
@@ -4,9 +4,9 @@
 #include "definitions.hpp"
 
 #if (PROVER_FORK_ID==10)
-#define ZKEVM_PROVER_VERSION "v7.0.0-RC0-fork.10"
+#define ZKEVM_PROVER_VERSION "v7.0.0-RC1-fork.10"
 #elif (PROVER_FORK_ID==11)
-#define ZKEVM_PROVER_VERSION "v7.0.0-RC0-fork.11"
+#define ZKEVM_PROVER_VERSION "v7.0.0-RC1-fork.11"
 #else
 #error "Invalid PROVER_FORK_ID"
 #endif
