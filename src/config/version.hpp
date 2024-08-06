c2a8e7611c3e659a6270ee08df329c481af9e46f
fractasy <fractasy@gmail.com>
2024-08-06 08:01:09 +0000

    Change version to v8.0.0-RC2

--- src/config/version.hpp
+++ src/config/version.hpp
@@ -4,9 +4,9 @@
 #include "definitions.hpp"
 
 #ifdef __AVX512__
-#define ZKEVM_PROVER_VERSION "v8.0.0-RC1.avx512"
+#define ZKEVM_PROVER_VERSION "v8.0.0-RC2.avx512"
 #else
-#define ZKEVM_PROVER_VERSION "v8.0.0-RC1"
+#define ZKEVM_PROVER_VERSION "v8.0.0-RC2"
 #endif
 
 #endif
